33f4a04f76ff08ba02baa953acf65a669d6ace73
Andres Alvarez <andresalvarez09092004@gmail.com>
2020-10-22 01:30:33 -0500

    Updates

---
+++ precache-manifest.b040006d111a59fb0b5ec36199b9b70d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb1a38295d3d8f8dcd9d75206331af95",
+    "url": "/Rick_Morty_API_React/index.html"
+  },
+  {
+    "revision": "8ec33f114aba9cefaca9",
+    "url": "/Rick_Morty_API_React/static/css/main.ce7df9b8.chunk.css"
+  },
+  {
+    "revision": "a8bda5a9958c6512f6e1",
+    "url": "/Rick_Morty_API_React/static/js/2.70f06b46.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/Rick_Morty_API_React/static/js/2.70f06b46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8ec33f114aba9cefaca9",
+    "url": "/Rick_Morty_API_React/static/js/main.b333e8f8.chunk.js"
+  },
+  {
+    "revision": "0141abde20d6124a32dc",
+    "url": "/Rick_Morty_API_React/static/js/runtime-main.6bd5c3d3.js"
+  },
+  {
+    "revision": "42d6efdb5571c7fc125c19f9d1ab2b3b",
+    "url": "/Rick_Morty_API_React/static/media/Rick_and_Morty_logo.42d6efdb.png"
+  },
+  {
+    "revision": "43b859b1d9eba8cf0962460d23214d99",
+    "url": "/Rick_Morty_API_React/static/media/learn-more.43b859b1.svg"
+  },
+  {
+    "revision": "7efec23661e049e412f92d131ed1d7e2",
+    "url": "/Rick_Morty_API_React/static/media/portal.7efec236.png"
+  }
+]);
